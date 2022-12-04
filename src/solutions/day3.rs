cdb46f85edde151d393f35e78f2657f688b473ae
Laurent Erreca <laurent.erreca@cfm.fr>
2022-12-04 15:30:38 +0100

    Day3 part 2

--- src/solutions/day3.rs
+++ src/solutions/day3.rs
@@ -10,11 +10,14 @@ pub fn solve(part: u8, input: &String) -> String {
     let tuples = alphabet.into_iter().enumerate().map(|(idx, letter)| (letter, idx+1)).collect::<Vec<(char, usize)>>();
     let m: HashMap<_, _> = tuples.into_iter().collect();
     let mut sm: usize = 0;
+    let mut global_nb_line: usize = 0;
+    let mut nb_line: usize = 0;
+    let mut buffer: Vec<HashSet::<char>> = vec![];
 
     for st in vecstr {
         let x = st.chars();
         let sz = x.clone().count();
-        let y: Vec<Vec<char>> = x
+        let y: Vec<Vec<char>> = x.clone()
                                   .into_iter()
                                   .chunks(sz/2)
                                   .into_iter()
@@ -24,12 +27,22 @@ pub fn solve(part: u8, input: &String) -> String {
         let compartiment_one = HashSet::<char>::from_iter(y[0].clone());
         let compartiment_two = HashSet::<char>::from_iter(y[1].clone());
         let res = compartiment_one.intersection(&compartiment_two).collect::<Vec<&char>>()[0];
-        //println!("{:?}", res);
-        sm += m.get(res).unwrap();
+        if part == 1 { sm += m.get(res).unwrap(); }
+        if part == 2 {
+            buffer.push(HashSet::<char>::from_iter(x));
+            if nb_line == 2 {
+                let r = &(&buffer[global_nb_line-2] & &buffer[global_nb_line-1]) & &buffer[global_nb_line];
+                let letter = &r.into_iter().collect::<Vec<char>>()[0];
+                sm += m.get(letter).unwrap();
+                nb_line = 0;
+            }
+            else { nb_line += 1; }
+            global_nb_line += 1;
+        }
 
     }
 
-    println!("Sum : {:?}", sm);
+    if part == 1 || part == 2 { return sm.to_string(); }
 
     return String::from("Wrong value for part");
 }
